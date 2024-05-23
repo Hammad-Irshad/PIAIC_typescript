8fb6c63206cb6cec0bdc42a8686e9789eb5b8a1a
Hammad-Irshad <hammadirshad19@gmail.com>
2024-05-23 14:44:19 +0500

    adding class work

---
+++ class/main.ts
@@ -0,0 +1,49 @@
+// // // type Student ={
+// // //     name : string
+// // //     rollNumber : number
+// // // }
+
+
+// // // let std1 : Student[] =[
+// // //     {
+// // //     name: "ali",
+// // //     rollNumber : 12345,
+// // // },
+// // // {
+// // //     name: "ahram",
+// // //     rollNumber : 12345,
+// // // },
+// // // {
+// // //     name: "osama",
+// // //     rollNumber : 12345,
+// // // }
+// // // ]
+
+
+// // // console.log(std1[1].name)
+
+
+
+
+// // //sclice
+
+
+
+// // let fruits : string[] = ["mango", "apple","banana","kiwi"];
+
+// // let dupfruits = fruits.slice(3,-1);
+
+// // console.log(dupfruits);
+
+
+
+// let fruits : string[] = ["mango", "apple","banana","kiwi"];
+
+// fruits.splice(1,2)
+
+// console.log(fruits)
+
+
+for(let i=0;i<15;i++){
+    console.log(i);
+}
